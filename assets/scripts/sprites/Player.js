aceb51584768b8ebf2d47a71c76af503ff9b2d6d
Daniil <kabzdec333@gmail.com>
2023-10-05 11:15:53 +0300

    update touch player controll, added offsets

--- assets/scripts/sprites/Player.js
+++ assets/scripts/sprites/Player.js
@@ -17,6 +17,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
     this.scene.physics.add.existing(this); //add the sprite to the physics
     this.scene.input.addPointer(3);
     this.body.enable = true;
+    this.touchControllInit = false;
     this.quantitySkins = shipsConfigs.length;
     this.currentSkinNumber = 1;
     this.worldOffset = 40;
@@ -28,7 +29,6 @@ export default class Player extends Phaser.GameObjects.Sprite {
     this.speed = speed;
     const cursorFollowSpeedCoefficient = 80000;
     this.cursorFollowSpeed = 1 / this.speed * cursorFollowSpeedCoefficient;
-    this.coursorInit = false;
 
     this.shots = new Shots(this.scene, this);
     this.attackFlag = true;
@@ -50,27 +50,24 @@ export default class Player extends Phaser.GameObjects.Sprite {
       duration: 800,
       repeat: 0,
       onComplete: () => {
-        this.coursorInit = true;
+        this.touchControllInit = true;
         this.keyboardControll();
       },
     });
   }
 
   keyboardControll() {
-    const indent = this.worldOffset;
-    const indentTop = this.worldOffsetTop;
-
-    if (this.scene.keyboard.up.isDown && this.y > indent + indentTop) {
+    if (this.scene.keyboard.up.isDown && this.y > this.worldOffset + this.worldOffsetTop) {
       this.body.setVelocityY(-this.speed);
-    } else if (this.scene.keyboard.down.isDown && this.y < this.scene.sys.game.config.height - indent) {
+    } else if (this.scene.keyboard.down.isDown && this.y < this.scene.sys.game.config.height - this.worldOffset) {
       this.body.setVelocityY(this.speed);
     } else {
       this.body.setVelocityY(0);
     }
 
-    if (this.scene.keyboard.left.isDown && this.x > indent) {
+    if (this.scene.keyboard.left.isDown && this.x > this.worldOffset) {
       this.body.setVelocityX(-this.speed);
-    } else if (this.scene.keyboard.right.isDown && this.x < this.scene.sys.game.config.width - indent) {
+    } else if (this.scene.keyboard.right.isDown && this.x < this.scene.sys.game.config.width - this.worldOffset) {
       this.body.setVelocityX(this.speed);
     } else {
       this.body.setVelocityX(0);
@@ -95,18 +92,22 @@ export default class Player extends Phaser.GameObjects.Sprite {
   touchControll() {
     const fingerOffsetZone = 100;
     const fingerIndent = 60;
-    const finderConditionX = Math.abs((this.x - fingerIndent) - this.scene.input.pointer1.x) < fingerOffsetZone;
-    const finderConditionY = Math.abs(this.y - this.scene.input.pointer1.y) < fingerOffsetZone;
+    const fingerConditionX = Math.abs((this.x - fingerIndent) - this.scene.input.pointer1.x) < fingerOffsetZone;
+    const fingerConditionY = Math.abs(this.y - this.scene.input.pointer1.y) < fingerOffsetZone;
 
-    if (this.scene.input.pointer1.isDown && this.coursorInit && !this.playerIsTouch && finderConditionX && finderConditionY) {
+    if (this.scene.input.pointer1.isDown && this.touchControllInit && !this.playerIsTouch && fingerConditionX && fingerConditionY) {
       this.playerIsTouch = true;
     }
 
-    if (!this.scene.input.pointer1.isDown && this.coursorInit && this.playerIsTouch) {
+    if (!this.scene.input.pointer1.isDown && this.touchControllInit && this.playerIsTouch) {
       this.playerIsTouch = false;
     }
 
-    if (this.scene.input.pointer1.isDown && this.coursorInit && this.playerIsTouch) {
+    const worldOffsetConditionTop = this.scene.input.pointer1.y > this.worldOffset + this.worldOffsetTop;
+    const worldOffsetConditionBottom = this.scene.input.pointer1.y < this.scene.sys.game.config.height - this.worldOffset;
+    const worldOffsetConditionX = Math.abs(this.scene.input.pointer1.x + fingerIndent) < this.scene.sys.game.config.width - this.worldOffset;
+
+    if (this.scene.input.pointer1.isDown && this.touchControllInit && this.playerIsTouch && worldOffsetConditionX && worldOffsetConditionTop && worldOffsetConditionBottom) {
       this.scene.tweens.add({
         targets: this,
         y: this.scene.input.pointer1.y,
@@ -116,7 +117,7 @@ export default class Player extends Phaser.GameObjects.Sprite {
       });
     }
 
-    if (this.scene.input.pointer2.isDown && this.coursorInit && this.attackFlagTouch && this.timeShotFlagTouch) {
+    if (this.scene.input.pointer2.isDown && this.touchControllInit && this.attackFlagTouch && this.timeShotFlagTouch) {
       this.shots.shot(this.speed);
       this.attackFlagTouch = false;
       this.timeShotFlagTouch = false;
